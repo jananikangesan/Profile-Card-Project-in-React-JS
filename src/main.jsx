d52dec1a69b1344038a8e51b726351221c34782b
jananikangesan <janakanges1996@gmail.com>
2024-08-09 09:26:30 +0530

    QR code generator with react

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 //import App from './App.jsx'
-import './index.css'
+//import './index.css'
+import './Qrcode.css'
 import { UserCard } from './UserCard'
+import { QrCode } from './QrCode'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
-    <UserCard/>
+    {/* <UserCard/> */}
+    <QrCode/>
   </React.StrictMode>,
 )
